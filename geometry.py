7279324ed303db0eecd34d16d6e2a92ffeae6648
michaelJwilson <mjwilson@lbl.gov>
2022-02-09 07:05:02 -0800

    add notebook deprecation.

---
+++ geometry.py
@@ -0,0 +1,35 @@
+import healpy as hp
+import numpy as np
+
+
+def radec2pix(ra, dec, nside=32):
+    theta = np.pi / 2. - np.radians(dec)
+    phi   = np.radians(ra)
+    
+    all_pixel_indices1 = hp.ang2pix(nside, theta, phi,nest=True, lonlat=False)
+
+    return pix
+
+def hp_props(nside):
+    npix = hp.nside2npix(nside)
+    pixel_area = hp.nside2pixarea(nside,degrees=True)
+
+    return npix, pixel_area
+
+def targ_hpmap(targs, norm=None, nside=32):
+    pix = radec2pix(ra, dec, nside=nside)
+    
+    #indice of filled pixels and corrosponding targets in pixel
+    filled_pixel_index, filled_targets_per_pixel = np.unique(pix, return_counts=True) 
+
+    #no. targets per pixel, initially 0 
+    target_pixel_density = np.zeros(hp.nside2npix(nside))
+
+    #update no. targets per pixel 
+    target_pixel_density[filled_pixel_index] = filled_targets_per_pixel 
+    target_pixel_density[target_pixel_density == 0] = np.NaN
+
+    if norm != None:
+        target_pixel_density /= norm
+    
+    return  target_pixel_density
