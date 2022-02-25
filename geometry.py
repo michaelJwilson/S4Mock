1d06d1926bf5a2bf0f3455fb7d0c27902ce6ef30
leahbigwood <leah.bigwood@durham.ac.uk>
2022-02-25 07:27:46 -0800

    added geometry.py

--- geometry.py
+++ geometry.py
@@ -1,12 +1,10 @@
-import healpy as hp
 import numpy as np
-from desitarget.geomask import get_imaging_maskbits
 
 
-def radec2pix(ra, dec, nside=32,unique=False):
+def radec2pix(ra, dec, nside=32, unique=False):
     theta = np.pi / 2. - np.radians(dec)
     phi   = np.radians(ra)
-    
+
     pix   = hp.ang2pix(nside, theta, phi, nest=True, lonlat=False)
 
     if unique:
@@ -17,33 +15,18 @@ def radec2pix(ra, dec, nside=32,unique=False):
 def hp_props(nside):
     npix = hp.nside2npix(nside)
     pixel_area = hp.nside2pixarea(nside,degrees=True)
-
     return npix, pixel_area
-
-def targ_hpmap(targs, norm=None, nside=32, return_pix=False):
-    pix = radec2pix(targs['RA'], targs['DEC'], nside=nside)
+def targ_hpmap(targs, norm=None, nside=32):
+    pix = radec2pix(ra, dec, nside=nside)
     
     #indice of filled pixels and corrosponding targets in pixel
     filled_pixel_index, filled_targets_per_pixel = np.unique(pix, return_counts=True) 
-
     #no. targets per pixel, initially 0 
     target_pixel_density = np.zeros(hp.nside2npix(nside))
-
     #update no. targets per pixel 
     target_pixel_density[filled_pixel_index] = filled_targets_per_pixel 
     target_pixel_density[target_pixel_density == 0] = np.NaN
-
-    if norm is not None:
-        
-        for i in range(len(norm)):
-            target_pixel_density[i] /= norm[i]
+    if norm != None:
+        target_pixel_density /= norm
     
-    if return_pix:
-        return target_pixel_density,pix
-    
-    else:
-        return  target_pixel_density
-
-
-
-    
+    return  target_pixel_density
